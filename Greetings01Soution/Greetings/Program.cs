ca7f7897457bca9d123616657ba4d1a7cea92b4c
st52558 <st52558@student.upce.cz>
2019-02-18 19:36:20 +0100

    konec

--- Greetings01Soution/Greetings/Program.cs
+++ Greetings01Soution/Greetings/Program.cs
@@ -8,8 +8,39 @@ namespace Greetings
 {
     class Program
     {
+        const uint default_count = 100;
+        static uint GetCount(string[] args)
+        {
+            if (args.Length == 0) return default_count;
+            if (uint.TryParse(args[0], out uint temp)) return temp;
+            return default_count;
+            
+        }
         static void Main(string[] args)
         {
+            /*Console.Write("Hello World");
+            Console.WriteLine("Hello World again");
+            
+            String greetings = "Hello World ";
+            for (uint i = 0; i < GetCount(args); i++)
+            {
+                greetings += "again ";
+            }
+            greetings = greetings.Remove(greetings.Length - 1);
+            greetings += "!";
+
+            Console.WriteLine(greetings);*/
+
+            StringBuilder greetingsBuilder = new StringBuilder();
+            greetingsBuilder.Append("Hello world again");
+            for (int i = 1; i < GetCount(args); i++)
+            {
+                greetingsBuilder.Append(" again");
+
+            }
+            greetingsBuilder.Append("!");
+            Console.WriteLine(greetingsBuilder.ToString());
+        
         }
     }
 }
---
+++ cv01Solution/cv01/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cv01
+{
+
+    class Program
+    {
+        static void RodneCislo(string rc)
+        {
+            if (rc[2] == '5' || rc[2] == '6')
+            {
+                Console.WriteLine("Žena");
+            }
+            else if (rc[2] == '1' || rc[2] == '0')
+            {
+                Console.WriteLine("Muž");
+            }
+            else
+            {
+                Console.WriteLine("Neplatný měsíc");
+            }
+        }
+        static void Main(string[] args)
+        {
+            //příklad 1
+            Console.WriteLine("Josef Novák");
+            Console.WriteLine("Jindrišská 16");
+            Console.WriteLine("111 50, Praha 1");
+
+            //příklad 2
+            for (int i = 0; i < 26; i++)
+            {
+                Console.Write(Convert.ToChar(i + 65));
+            }
+
+            //příklad 3
+            string rc = "5655203565";
+            RodneCislo(rc);
+
+            //příklad 4
+            Random RNG = new Random();
+            int cislo = RNG.Next(0,100);
+            int hadani = int.Parse(Console.ReadLine());
+            while (cislo != hadani)
+            {
+                
+                if (cislo > hadani)
+                {
+                    Console.WriteLine("přidej");
+                } else
+                {
+                    Console.WriteLine("uber");
+                }
+                hadani = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("uhodnuto");
+            Console.WriteLine(cislo);
+
+            //příklad 5
+        }
+    }
+}
